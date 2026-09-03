677f0017832ac9887c3549c31c7e3a9a12be4a6a
Rory Gianni <hello@rgianni.cc>
2026-09-03 16:54:10 +0100

    Relabel scripts and makes available alternative backend report and summarise commands in the quick submit buttons when creating new jobs

--- app/components/admin/sensemaker/new_component.rb
+++ app/components/admin/sensemaker/new_component.rb
@@ -3,6 +3,40 @@ class Admin::Sensemaker::NewComponent < ApplicationComponent
 
   attr_reader :sensemaker_job
 
+  QUICK_ACTIONS = {
+    summary: { default_backend: :node },
+    report: { default_backend: :node }
+  }.freeze
+
+  def quick_action_label(logical_name)
+    case logical_name
+    when :summary
+      I18n.t("admin.sensemaker.new.generate_summary")
+    when :report
+      I18n.t("admin.sensemaker.new.generate_report")
+    end
+  end
+
+  def quick_action_scripts(logical_name)
+    scripts = Sensemaker::ScriptRegistry.scripts_for_logical_name(logical_name)
+    preferred = QUICK_ACTIONS.fetch(logical_name)[:default_backend]
+    default = scripts.find { |script| Sensemaker::ScriptRegistry.backend_for(script) == preferred }
+
+    ([default] + scripts).compact.uniq
+  end
+
+  def script_option_label(script)
+    key = Sensemaker::ScriptRegistry.i18n_key(script)
+    I18n.t("admin.sensemaker.scripts.#{key}.title")
+  end
+
+  def script_options
+    Sensemaker::ScriptRegistry.user_selectable.map do |script|
+      key = Sensemaker::ScriptRegistry.i18n_key(script)
+      [I18n.t("admin.sensemaker.scripts.#{key}.title"), script]
+    end
+  end
+
   def initialize(sensemaker_job, search_results, result_count)
     @sensemaker_job = sensemaker_job
     @search_results = search_results
@@ -18,12 +52,5 @@ class Admin::Sensemaker::NewComponent < ApplicationComponent
 
   def title
     t("admin.sensemaker.new.title")
-  end
-
-  def script_options
-    Sensemaker::ScriptRegistry.user_selectable.map do |script|
-      key = Sensemaker::ScriptRegistry.i18n_key(script)
-      [I18n.t("admin.sensemaker.scripts.#{key}.title"), script]
-    end
   end
 end
